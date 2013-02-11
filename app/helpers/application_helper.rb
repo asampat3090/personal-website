3bfad6ddd77d6f1e5b6b16f36f02f78816ad1a6c
Anand Sampat <asampat3090@gmail.com>
2013-02-10 23:47:51 -0800

    created simple import of blog from wordpress site - first pass - no formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,17 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # load the blog feed
+
+  def load_blog_feed
+    feed = Feedzirra::Feed.fetch_and_parse("http://anandsampat.wordpress.com/feed");
+    unless feed.is_a?(Fixnum)
+      @feed_title = feed.title;
+      @blog_posts = feed.entries[0...3]; nil
+    else
+      @blog_posts = []
+    end
+  end
+
 end
