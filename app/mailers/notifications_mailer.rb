336dfbe9292ea9b9893cab8f9391dc60c153bda0
Anand Sampat <asampat3090@gmail.com>
2013-02-28 20:13:54 -0800

    changed subject header to show website name

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -5,7 +5,7 @@ class NotificationsMailer < ActionMailer::Base
 
   def new_message(message)
     @message = message
-    mail(:subject => "[YourWebsite.tld] #{message.subject}")
+    mail(:subject => "[AnandSampat.com] #{message.subject}")
   end
 
 end
